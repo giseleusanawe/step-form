2c2664660ee209b68e42e1b7811ab43034c9370d
Gisele <giselleusanase@gmail.com>
2023-04-30 17:01:06 +1300

    steps

---
+++ index.js
@@ -0,0 +1,21 @@
+function one() {
+    document.getElementById("form").style.display= "block";
+    document.getElementById("step-2").style.display = "none";
+    document.getElementById("thank-you").style.display = "none";
+}
+function two() {
+    document.getElementById("step-2").style.display = "block";
+    document.getElementById("form").style.display= "none";
+    
+  }
+  function four() {
+    document.getElementById("step-2").style.display = "block";
+    document.getElementById("form").style.display= "none";
+    document.getElementById("thank-you").style.display = "none";
+  }
+  function four() {
+    document.getElementById("thank-you").style.display = "block";
+    document.getElementById("form").style.display= "none";
+    document.getElementById("step-2").style.display = "none";
+    
+  }
